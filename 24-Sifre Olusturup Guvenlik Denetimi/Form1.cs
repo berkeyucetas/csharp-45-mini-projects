a9592bfa9c981388936dee5e2d0fd766c931a1c2
berkeyucetas <116177776+berkeyucetas@users.noreply.github.com>
2026-05-06 18:07:08 +0300

    Add files via upload

---
+++ 24-Sifre Olusturup Guvenlik Denetimi/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Şifre_Oluşturup_Güvenlik_Denetimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int karaktersayisi = textSifre.Text.Length;
+            if (karaktersayisi > 0 && karaktersayisi < 4)
+            {
+                progressBar1.Value = 1;
+                progressBar1.ForeColor = Color.Red;
+                label3.Text = "Zayıf";
+            }
+            else if (karaktersayisi >= 4 && karaktersayisi < 8)
+            {
+                progressBar1.Value = 2;
+                progressBar1.ForeColor = Color.Yellow;
+                label3.Text = "Orta";
+            }
+            else if (karaktersayisi >= 8)
+            {
+                progressBar1.Value = 3;
+                progressBar1.ForeColor = Color.Green;
+                label3.Text = "Güçlü";
+            }
+            else
+            {
+                MessageBox.Show("Şifre Oluşturulamadı..!");
+
+                progressBar1.Value = 0;
+            }
+
+        }
+    }
+}
