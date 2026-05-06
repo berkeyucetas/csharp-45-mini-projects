e5a2bb8bc6d0e55a5c5eb749e513b3e596dc1a0f
berkeyucetas <116177776+berkeyucetas@users.noreply.github.com>
2026-05-06 18:07:36 +0300

    Add files via upload

---
+++ 25-Metin Sifreleme/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metin_Şifreleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string anametin= textBox1.Text;
+            byte[] veridizim = ASCIIEncoding.UTF8.GetBytes(anametin);
+            string sifrelimetin=Convert.ToBase64String(veridizim);
+            textBox2.Text= sifrelimetin;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string metincoz= textBox2.Text;
+            byte[] vericoz= Convert.FromBase64String(metincoz);
+            string anametin2= ASCIIEncoding.ASCII.GetString(vericoz);
+            textBox1.Text= anametin2;
+        }
+    }
+}
