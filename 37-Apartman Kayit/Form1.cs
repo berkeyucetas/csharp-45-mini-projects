1e388d7f6fb13de91695540dc7100069b4614188
berkeyucetas <116177776+berkeyucetas@users.noreply.github.com>
2026-05-15 22:46:53 +0300

    Add files via upload

---
+++ 37-Apartman Kayit/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Apartman_Kayıt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButtonGiris_Click(object sender, EventArgs e)
+        {
+            if (textKullaniciAd.Text == "Yönetici" && textSifre.Text == "admin13579")
+            {
+                frmAnaSayfa form= new frmAnaSayfa();
+                form.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı kullanıcıadı veya şifre");
+            }
+        }
+    }
+}
---
+++ 37-Apartman Kayit/frmAnaSayfa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Apartman_Kayıt
+{
+    public partial class frmAnaSayfa : Form
+    {
+        public frmAnaSayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            frmDaireler form= new frmDaireler();
+            form.ShowDialog();
+        }
+    }
+}
